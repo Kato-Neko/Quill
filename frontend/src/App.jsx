f7291b61ba4b7193840ce689958d2f8f763be42b
Jermaine Gadiano <152935237+Dazzzaiii@users.noreply.github.com>
2025-09-19 02:28:03 +0800

    Create Note Page UI (create, edit, delete buttons)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotePage from "./pages/NotePage";
 import NotesList from "./pages/NotesList";
 
 function App() {
   return (
     <Router>
-      <nav className="flex gap-4 p-4 bg-gray-100 shadow">
-        <Link to="/" className="text-blue-600">Notes List</Link>
-        <Link to="/note" className="text-blue-600">Note Page</Link>
-      </nav>
-
       <Routes>
         <Route path="/" element={<NotesList />} />
         <Route path="/note" element={<NotePage />} />
+        <Route path="/note/:id" element={<NotePage />} />
       </Routes>
     </Router>
   );
